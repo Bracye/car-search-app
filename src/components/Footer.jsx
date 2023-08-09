ee98a3f9d3fde33fbf4a153b40c3d01d5be9983c
Bracye <ernestbracy1@gmail.com>
2023-08-09 15:55:06 -0400

    Updated structure of component details and styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootswatch/dist/cyborg/bootstrap.min.css'
 
 function Footer() {
   return (
+    //
     <footer className="text-center py-3" style={{ fontFamily: 'Staatliches' }}>
       <p className='fs-5'>Car Search App &copy; {new Date().getFullYear()} - Ernest Bracy</p>
     </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Header() {
   return (
-    <header className="py-5">
+    //App title with some styling
+    <header className='my-4'>
 <h1 className="text-center" style={{ fontFamily: 'Staatliches, regular', textDecoration: 'underline', margin: 0 }}>
           Car Search App
         </h1>
-  )
 
     </header>
     
