7913e601dd3c3a3c537c55b5bda056db33dea23c
Bracye <ernestbracy1@gmail.com>
2023-08-10 10:08:48 -0400

    Added a nav component to hold links

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,17 @@ import MotorcycleSearch from './pages/MotorcycleSearch';
 import Main from './pages/Main';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div>
       <Header />
+      <Nav />
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/cars/*" element={<CarSearch />} />
-        <Route path="/motorcycles/*" element={<MotorcycleSearch />} />
+        <Route path="/cars/" element={<CarSearch />} />
+        <Route path="/motorcycles/" element={<MotorcycleSearch />} />
       </Routes>
       <Footer />
     </div>
