45267eea5f9a14f6a4e3121446bc1da5d99b448a
Bracye <ernestbracy1@gmail.com>
2023-08-11 15:18:26 -0400

    fixed app description

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -6,7 +6,7 @@ function Main() {
   return (
     <div style={{ textAlign: 'center', fontFamily: 'Staatliches, sans-serif' }}>
       <img src={tireImage} alt="Tire Image" style={{ maxWidth: '400px', height: 'auto', margin: '40px' }} />
-      <h2 className='mb-5 py-4 fs-1'>This app allows you to search for cars and motorcycles based on make and model.</h2>
+      <h2 className='mb-5 py-4 fs-1'>This app allows you to search for vehicles based on make and model.</h2>
       <h3 className='fs-2'>Start searching with the following options:</h3>
       <div className="d-flex justify-content-center">
         <div className="btn-group" role="group">
